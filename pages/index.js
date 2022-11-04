39d909cf90159bda60b3a4a1fd83820942764282
kiransantha <kiransantha@gmail.com>
2022-11-04 18:25:23 +0530

    added portfolio heading

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,29 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import React, {useState} from 'react';
 
 const cmb = '#0b74fd', clb = '#00bffe', cdg = '#0d4c3c',  cdr = '#981f29';
 const cw = '#fefefe', cg = '#656c7b', cdb = '#0e172f';
 
 const header_background = cw; 
 const greetings_heading = clb, greetings_role = cg, greetings_description = cdb;
+const hr_color = cdb, portfolio_heading = cdg, portfolio_category_text = cmb;
+const portfolio_category_text_hover = cdr, portfolio_category_text_active = cdr;
 
 const home_page_url = 'https://kiransantha.github.io/';
 const description = "Kiran Sai Santa";
 
-export default function Home({ postsMetaData }) {
+export default function Home() {
+  const projec_dirs = [''];
+  const [selectedCategory, setSelectedCategory] = useState(0);
+  const category_names = ['All Projects', 'Interior', 'Exterior', 'Animation'];
+
   return (
     <div>
       <Head>
         <meta charSet="utf-8" />
-        <meta name = "viewport" content = "width=device-width, initial-scale=1" />
-        <meta name = 'description' content = {description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name='description' content={description} />
         <meta name="author" content="Kiran Sai Santa" />
         <meta name="keywords" content={'3d visualization,portfolio,architecture,interior design'} />
 
@@ -24,21 +31,41 @@ export default function Home({ postsMetaData }) {
       </Head>
       
       <div>
-        <div className = 'header-info'>
-          <div className = 'profile-section'>
-            <img className = 'profile-image' src = '/images/profile/kiran-sai-santa.jpg'
-                  alt = 'Kiran Sai Santa'/>
+        {/* Header section */}
+        <div className='header-info'>
+          <div className='profile-section'>
+            <img className='profile-image' src='/images/profile/kiran-sai-santa.jpg'
+                  alt='Kiran Sai Santa'/>
           </div>
-
-          <div className = 'greetings-section'>
-            <p className = 'greetings-heading'>Kiran Sai Santa, <span className = 'role-type'>IGBC AP Associate</span></p>
-            <p className = 'greetings-role'>3D VISUALIZER</p>
-            <p className = 'greetings-description'>Focused on creating Exterior Facades, 
+          <div className='greetings-section'>
+            <p className='greetings-heading'>Kiran Sai Santa, <span className='role-type'>IGBC AP Associate</span></p>
+            <p className='greetings-role'>3D VISUALIZER</p>
+            <p className='greetings-description'>Focused on creating Exterior Facades, 
             Interior Designing, Elevations, and Creative Spaces.
             </p>
-            <p className = 'greetings-description'>High interest in Sustainability Planning, and Designing.
+            <p className='greetings-description'>High interest in Sustainability Planning, and Designing.
             Knowledge of Sustainable Design Elements and IGBC Green Building Standards. 
             </p>
+          </div>
+        </div>
+
+        <div className='horizontal-line'></div>
+
+        {/* Portfolio section*/}
+        <div className='portfolio-section'>
+          <p className='portfolio-heading'>3D VISUALIZATION PORTFOLIO</p>
+          {/* Portfolio section categories */}
+          <div className='portfolio-section-categories'>
+            {category_names.map((name, index) => {
+                return (
+                  <a className='portfolio-category-text'
+                    style={{color: selectedCategory==index? portfolio_category_text_active:portfolio_category_text}}
+                    onClick={(event)=>setSelectedCategory(index)}>
+                    {name}
+                  </a>
+                )
+              }
+            )}
           </div>
         </div>
 
@@ -53,7 +80,6 @@ export default function Home({ postsMetaData }) {
           display: flex;
           flex: 100%;
           flex-direction: row;
-          background: ${header_background};
           padding: 2vh 0vw 1vh 0vw;
         }
 
@@ -85,7 +111,6 @@ export default function Home({ postsMetaData }) {
           font-family: 'Alegreya Sans', sans-serif;
           font-weight: bold;
           font-size: 3em;
-          text-align: center;
           color: ${greetings_heading};
         }
 
@@ -106,6 +131,49 @@ export default function Home({ postsMetaData }) {
           font-size: 2em;
           padding-top: 1vh;
           color: ${greetings_description}
+        }
+
+        .horizontal-line {
+          margin: 2vh 10% 2vh 10%;
+          border: 1px solid ${hr_color};
+        }
+
+        .portfolio-section {
+          display: flex;
+          flex: 100%;
+          flex-direction: column;
+          padding: 2vh 10% 1vh 10%;
+        }
+
+        .portfolio-heading {
+          font-family: 'Alegreya Sans', sans-serif;
+          font-weight: bold;
+          font-size: 2.5em;
+          text-align: center;
+          color: ${portfolio_heading};
+        }
+
+        .portfolio-section-categories {
+          display: flex;
+          flex-direction: row;
+          flex: 100%;
+          justify-content: center;
+          padding: 1vh 0 1vh 0;
+        }
+
+        .portfolio-category-text {
+          font-family: 'Source Sans Pro', sans-serif;
+          font-size: 1.5em;
+          text-decoration: none;
+          padding: 0 2% 0 2%;
+        }
+
+        .portfolio-category-text:hover {
+          color: ${portfolio_category_text_hover};
+        }
+
+        .portfolio-category-text:active {
+          color: ${portfolio_category_text_active};
         }
       `}</style>
     </div>
